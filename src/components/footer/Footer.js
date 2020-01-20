bf1e327ab817738e7dec08cfb0a6cfcdc3f8b46d
Kevin Cho <hcho163@aucklanduni.ac.nz>
2020-01-20 21:16:57 +1300

    created basic scss folder and component folder structures

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+
+    render() {
+        return (
+            <footer className="footer__container">
+                Some footer content
+            </footer>
+        )
+    }
+}
