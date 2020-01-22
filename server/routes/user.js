4749bac84cbc8db40fdbcb5d6727c582945661ec
Jun Park <jun.y.park17@gmail.com>
2020-01-22 23:15:09 +1300

    Register User API
    With input validation. Moved password hash/salt logic to the api. Probably needs to go elsewhere.

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,76 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../controllers/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/dev");
+const { check, validationResult } = require("express-validator");
 
-router.post("/register", User.register);
-router.post("/auth", User.authenticate);
+const User = require("../models/user");
+
+// router.post("/register", User.register);
+// router.post("/auth", User.authenticate);
+
+// @route   POST api/user
+// @desc    Registers a new user if the email is unique, and returns the user's token
+// @access  Public
+router.post(
+  "/",
+  // Validate input data
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password must be 6 or more characters").isLength({
+      min: 6
+    })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      user = new User({
+        email,
+        password
+      });
+
+      // Encrypt password with bcrypt
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(user.password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id //abstraction in mongoose means this doesn't need _id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.jwtSecret,
+        { expiresIn: 36000 }, // temporary, change to 1 hour later
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
